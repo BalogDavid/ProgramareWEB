a5dbb08374fcea2bb767f0b2170888db058bc3c1
Balog David <balogdavid2015@gmail.com>
2024-06-02 23:00:43 +0300

    Update user_database.sql

--- public/user_database.sql
+++ public/user_database.sql
@@ -1,10 +1,8 @@
--- Crearea bazei de date
+
 CREATE DATABASE user_database;
 
--- Utilizarea bazei de date create
 USE user_database;
 
--- Crearea tabelului utilizatorilor
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
